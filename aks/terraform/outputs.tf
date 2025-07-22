ad6f16d0d9aa22dffa53ab5e6b7354e88295e4b2
noman <chnoman1213@gmail.com>
2025-07-22 20:54:46 +0500

    update output file

--- aks/terraform/outputs.tf
+++ aks/terraform/outputs.tf
@@ -17,11 +17,11 @@ output "resource_group_name" {
 }
 
 output "postgresql_server_name" {
-  value = azurerm_postgresql_server.this.name
+  value = azurerm_postgresql_flexible_server.this.name
 }
 
 output "postgresql_server_fqdn" {
-  value = azurerm_postgresql_server.this.fqdn
+  value = azurerm_postgresql_flexible_server.this.fqdn
 }
 
 output "redis_hostname" {
