bb18ea0d08b9efca17e392f127c524cf6f4c7421
noman <chnoman1213@gmail.com>
2025-07-28 18:16:40 +0500

    Fix EKS version to 1.29 and improve External Secrets CRD installation

--- eks/terraform/main.tf
+++ eks/terraform/main.tf
@@ -62,7 +62,7 @@ module "eks" {
   version = "18.31.2"
 
   cluster_name    = "my-eks-cluster"
-  cluster_version = "1.32"
+  cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
